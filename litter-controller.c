61d8431c6f79f351101a0542c4d66ca2229e39d5
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-02-27 18:50:38 -0800

    Use argparse library

--- litter-controller.c
+++ litter-controller.c
@@ -11,6 +11,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include <argparse.h>
+
 #define AF_BASE 64
 #define AF_RS (AF_BASE + 0)
 #define AF_RW (AF_BASE + 1)
@@ -21,25 +23,36 @@
 #define AF_DB6 (AF_BASE + 6)
 #define AF_DB7 (AF_BASE + 7)
 
-#define emptyingLed 27
-#define waitingLed 6
-#define dumpingLed 26
-#define errorLed 0
-
-#define emptyButton 23
-#define dumpButton 22
-
-#define clockwise 28
-#define counterclockwise 29
-
-#define trig 24
-#define echo 25
-
-#define emptyDistance 20
-#define poopingTime 60000
+static int emptyingLed =        27;
+static int waitingLed =         6;
+static int dumpingLed =         26;
+static int errorLed =           0;
+static int emptyButton =        23;
+static int dumpButton =         22;
+static int clockwise =          28;
+static int counterclockwise =   29;
+static int trig =               24;
+static int echo =               25;
+static int lcdWidth =           16;
+static int emptyDistance =      35;
+static int deltaDistance =      15;
+static int poopingTime =        180;
+static int DEBUG =              1;
 
 static char *emptyLcdLine = "                ";
 static int lcdHandle;
+
+
+static const char *const usage[] = {
+    "litter-controller [options] [[--] args]",
+    "litter-controller [options]",
+    NULL,
+};
+
+void debug(char *args) {
+    if (DEBUG)
+        printf("DEBUG: %s\n", args);
+}
 
 float sonic(void) {
     digitalWrite(trig, LOW);
@@ -82,7 +95,7 @@ int lcdWrite(bool clear, char *messageTop, char *messageBottom) {
         lcdClear(lcdHandle);
 
     if (strcmp(messageTop, "")) {
-        if (strlen(messageTop) > 16) {
+        if (strlen(messageTop) > lcdWidth) {
             printf("%s is too long; TODO scrolling", messageTop);
         } else {
             lcdPosition(lcdHandle, 0, 0);
@@ -93,20 +106,22 @@ int lcdWrite(bool clear, char *messageTop, char *messageBottom) {
     }
 
     if (strcmp(messageBottom, "")) {
-        lcdPosition(lcdHandle, 0, 1);
-        lcdPuts(lcdHandle, emptyLcdLine);
-        lcdPosition(lcdHandle, 0, 1);
-        lcdPuts(lcdHandle, messageBottom);
+        if (strlen(messageTop) > lcdWidth) {
+            printf("%s is too long; TODO scrolling", messageBottom);
+        } else {
+            lcdPosition(lcdHandle, 0, 1);
+            lcdPuts(lcdHandle, emptyLcdLine);
+            lcdPosition(lcdHandle, 0, 1);
+            lcdPuts(lcdHandle, messageBottom);
+        }
     }
 
     return 0;
 }
 
-void turnOnRelay(int direction, int time) {
-    printf("Turning on relay %d for %dms\n", direction, time);
-
+void turnOnRelay(int direction, int seconds) {
     digitalWrite(direction, LOW);
-    delay(time);
+    delay((seconds * 1000));
     digitalWrite(direction, HIGH);
 }
 
@@ -116,12 +131,12 @@ void dumpBox(char *source) {
     struct tm *time = localtime(&now);
     char buffer[26];
     strftime(buffer, 26, "%a %H:%M:%S", time);
-    lcdWrite(1, "Last dump:", buffer);
+    lcdWrite(1, "Last dumped:", buffer);
 
     digitalWrite(dumpingLed, HIGH);
 
-    turnOnRelay(clockwise, 1000);
-    turnOnRelay(counterclockwise, 1000);
+    turnOnRelay(clockwise, 10);
+    turnOnRelay(counterclockwise, 10);
 
     digitalWrite(dumpingLed, LOW);
 }
@@ -134,7 +149,14 @@ void emptyBox(char *source) {
     strftime(buffer, 26, "%a %H:%M:%S", time);
     lcdWrite(1, "Last empty:", buffer);
 
-    digitalWrite(emptyingLed, HIGH);
+    if (digitalRead(emptyButton) == HIGH) {
+        printf("Button called to EMPTY box at %s", ctime(&now));
+        emptyBox("button");
+    } else if (digitalRead(dumpButton) == HIGH) {
+        printf("Button called to DUMP box at %s", ctime(&now));
+        dumpBox("button");
+    }
+}
 
     turnOnRelay(counterclockwise, 3000);
     turnOnRelay(clockwise, 4000);
@@ -180,7 +202,7 @@ void waitForEvents(void) {
     }
 }
 
-void setPinModes() {
+void setPins() {
     pinMode(emptyingLed,        OUTPUT);
     pinMode(waitingLed,         OUTPUT);
     pinMode(dumpingLed,         OUTPUT);
@@ -194,12 +216,47 @@ void setPinModes() {
 
     pinMode(echo,               INPUT);
     pinMode(trig,               OUTPUT);
+
+    digitalWrite(emptyingLed,       LOW);
+    digitalWrite(waitingLed,        LOW);
+    digitalWrite(dumpingLed,        LOW);
+    digitalWrite(errorLed,          LOW);
+
+    digitalWrite(clockwise,         HIGH);
+    digitalWrite(counterclockwise,  HIGH);
 }
 
-int main(void) {
+int main(int argc, const char **argv) {
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_GROUP("GPIO pins"),
+        OPT_INTEGER('e', "emptyingLed",     &emptyingLed,       "LED that glows when emptying"),
+        OPT_INTEGER('w', "waitingLed",      &waitingLed,        "LED that glows when waiting for exit"),
+        OPT_INTEGER('d', "dumpingLed",      &dumpingLed,        "LED that glows when dumping litter"),
+        OPT_INTEGER('r', "errorLed",        &errorLed,          "LED that glows on error"),
+        OPT_INTEGER('E', "emptyButton",     &emptyButton,       "button to initiate emptying"),
+        OPT_INTEGER('D', "dumpButton",      &dumpButton,        "button to initiate litter dumping"),
+        OPT_INTEGER('c', "clockwiseRelay",  &clockwise,         "clockwise relay pin"),
+        OPT_INTEGER('C', "cclockwiseRelay", &counterclockwise,  "counterclockwise relay pin"),
+        OPT_INTEGER('t', "trig",            &trig,              "ultrasonic sensor's trig pin"),
+        OPT_INTEGER('h', "echo",            &echo,              "ultrasonic sensor's echo pin"),
+        OPT_GROUP("Other options"),
+        OPT_INTEGER('l', "lcdWidth",        &lcdWidth,          "character width of the LCD screen"),
+        OPT_INTEGER('i', "emptyDistance",   &emptyDistance,     "distance (in cm) expected when empty"),
+        OPT_INTEGER('I', "deltaDistance",   &deltaDistance,     "distance (in cm) delta to trigger emptying"),
+        OPT_INTEGER('p', "poopingTime",     &poopingTime,       "time (in seconds) to wait before emptying"),
+        OPT_INTEGER('g', "debug",           &DEBUG,             "whether to show debugging output"),
+        OPT_END(),
+    };
+
+    struct argparse argparse;
+    argparse_init(&argparse, options, usage, 0);
+    argparse_describe(&argparse, "\nReplacement litter box controller.", "\nReplacement litter box controller expecting an RPI 3B+.");
+    argc = argparse_parse(&argparse, argc, argv);
+
     wiringPiSetup();
     lcdHandle = lcdSetup();
-    setPinModes();
+    setPins();
 
     lcdWrite(1, "henlo", "");
 
